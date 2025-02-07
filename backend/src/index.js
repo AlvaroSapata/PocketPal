cb8a1221b5576e1e346b28c7ee99127dd9676cf4
Alvaro Martinez <alvaromfdv@gmail.com>
2025-02-08 00:39:50 +0100

    protectRoute + cloudinary config

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-import {connectDB} from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
@@ -12,8 +13,9 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use(cookieParser());
 
 app.listen(PORT, () => {
-  console.log("Server is running on PORT:"+PORT);
+  console.log("Server is running on PORT:" + PORT);
   connectDB();
 });
