570447537c14e90fcbb4d93f41a8bf4af678a921
Alvaro Martinez <alvaromfdv@gmail.com>
2025-02-09 19:10:13 +0100

    TailwindCSS 4 + Daisyui(5 beta)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      {" "}
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-neutral">Neutral</button>
+    </div>
+  );
+};
 
-export default App
+export default App;
