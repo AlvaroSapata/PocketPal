2ef92f8d6d42c67867b784a773d1a8da0cce4e5a
Alvaro Martinez <alvaromfdv@gmail.com>
2025-02-10 14:21:20 +0100

    Increased request limit to 10 mb(back)

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,10 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
+// Increased request limit to 10mb
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 
