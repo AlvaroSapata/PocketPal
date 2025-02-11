a6e6a3145a272a66399266c0173dcf3945f336b5
Alvaro Martinez <alvaromfdv@gmail.com>
2025-02-11 13:34:45 +0100

    Offline / Online indicators

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -11,4 +11,23 @@ const io = new Server(server, {
   },
 });
 
+// Store online users
+const userShocketMap={}
+
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if(userId) userShocketMap[userId]=socket.id
+
+  //io.emit() to send events to all connected users
+  io.emit("getOnlineUsers", Object.keys(userShocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+    delete userShocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userShocketMap));
+  });
+});
+
 export { io, app, server };
